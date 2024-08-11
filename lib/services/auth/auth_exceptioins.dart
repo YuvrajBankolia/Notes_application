492362028ecb400ee56357804da803c20955286a
YuvrajBankolia <yuvrajbankolia@gmail.com>
2024-08-11 18:28:23 +0530

    commit 5

---
+++ lib/services/auth/auth_exceptioins.dart
@@ -0,0 +1,17 @@
+//  Login Exceptions
+class UserNotFoundAuthException implements Exception {}
+
+class WrongPasswordAuthException implements Exception {}
+//   register exceptions
+
+class WeakPasswordAAuthExceptions implements Exception {}
+
+class EmailAlreadyInUsedAuthExceptio implements Exception {}
+
+class InvalidEmailAuthException implements Exception {}
+
+// generic ewxceptions
+
+class GenericAuthException implements Exception {}
+
+class UserNotLoggedInAuthException implements Exception {}
